b6c7b4d428d5e795b9c4c5763a945afa306d5c6a
Jim Filippou <jimfilippou8@gmail.com>
2020-03-22 02:39:50 +0200

    Multiple queries

--- utils/query.go
+++ utils/query.go
@@ -9,28 +9,62 @@ import (
 	"context"
 	"github.com/olivere/elastic/v7"
 	"github.com/urfave/cli"
+	"io/ioutil"
+	"strings"
 )
 
-func Query(ctx *cli.Context, queriesPath string) (*elastic.SearchResult, error) {
+// This function is devoted 100% for my friend Artemis
+func fetchQueries(path string) ([]string, error) {
+
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var queries []string
+
+	tokens := strings.Split(string(file), "\n")
+
+	for i := 1; i <= len(tokens); i += 3 {
+		queries = append(queries, tokens[i])
+	}
+
+	return queries, nil
+
+}
+
+func Query(ctx *cli.Context, queriesPath string) ([]*elastic.SearchResult, error) {
 
 	client, err := elastic.NewSimpleClient(elastic.SetURL("http://127.0.0.1:9200"))
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO: Read queries file, iterate through queries & perform them
-	matchQuery := elastic.NewMatchQuery("Text", "query")
-
-	// This line is too JavaScripty, why the fuck did you make it like this?
-	searchResult, err := client.Search().
-		Index("ire").
-		Query(matchQuery).
-		Pretty(true).
-		Do(context.Background())
-
+	queries, err := fetchQueries(queriesPath)
 	if err != nil {
 		return nil, err
 	}
 
-	return searchResult, nil
+	var results []*elastic.SearchResult
+
+	for _, query := range queries {
+
+		matchQuery := elastic.NewMatchQuery("Text", query)
+
+		// This line is too JavaScripty, why the fuck did you make it like this?
+		searchResult, err := client.Search().
+			Index("ire").
+			Query(matchQuery).
+			Pretty(true).
+			Do(context.Background())
+		if err != nil {
+			return nil, err
+		}
+
+		results = append(results, searchResult)
+
+	}
+
+	return results, nil
+
 }
